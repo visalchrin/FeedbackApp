14c883cf1beffb87fdb32676460d531423153e4c
visalchrin <visalchrin96@gmail.com>
2021-09-23 11:10:56 +0700

    Hello world

--- feedback/settings.py
+++ feedback/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -142,3 +143,6 @@ MEDIA_URL = '/media/'
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
